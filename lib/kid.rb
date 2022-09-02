9b5e3313339aa29e7103563a3b1806c8eab86aeb
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-01 20:24:11 -0400

    Files working with ruby

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
+require_relative './fancy_dance'
 require_relative './meta_dancing_module'
+
+class Kid
+  extend MetaDancing
+  include Dance
+  extend FancyDance::InstanceMethods
+  include FancyDance::ClassMethods
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
